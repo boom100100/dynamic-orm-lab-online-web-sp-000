bf796b3f9cfc68e8a83db94739530fe8cb47ca67
Bernadette Davis <bsd231@nyu.edu>
2019-09-03 20:11:39 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,10 +7,11 @@ class Student < InteractiveRecord
   attr_accessor :id, :name, :grade
 
   def initialize
+=begin
     @id = hash[:id]
     @name = hash[:name]
     @grade = hash[:grade]
-
+=end
   end
 
 end
